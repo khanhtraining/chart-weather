540a6e58a3eefa2a61b94b44a2fdab93bbf1023a
ADMIN\Admin <huynhngockhanh0568@gmail.com>
2021-08-02 14:06:29 -0500

    Update chart weather: rect infor, rect moon, resize it.
    (cherry picked from commit c6fb84476ff87025fc96cc4be80434b1046a0ff6)

--- src/scenes/ChartContainer/Chart.js
+++ src/scenes/ChartContainer/Chart.js
@@ -10,7 +10,7 @@ export const Chart = (props) => {
         var g = Svg.append("g")
             .attr('class', 'weather-container')
         var x = d3.scalePoint()
-            .domain(((props.chartData).map(function (d) { return d.hour; })))
+            .domain(((props.chartData).map(function (d) { return d.hour })))
         var y = d3.scaleLinear()
             .domain([0, 3])
         var xAxis = g.append("g")
@@ -19,26 +19,13 @@ export const Chart = (props) => {
         var lineSunColor = 'orange'
 
         var lineTidePath = d3.area()
-            .x(function (d) { return x(d.hour); })
-            .y(function (d) { return y(d.tide); })
+            .x(function (d) { return x(d.hour) })
+            .y(function (d) { return y(d.tide) })
             .curve(d3.curveBumpX),
             lineSunPath = d3.area()
-                .x(function (d) { return x(d.hour); })
-                .y(function (d) { return y(d.sun); })
-                .curve(d3.curveMonotoneX);
-
-        const rectangle = () => {
-            g.append('path')
-                .data((props.chartData))
-                .attr("class", 'rect-line')
-                .attr("x", function (d) { return x(d.hour); })
-                .attr("y", function (d) { return y(d.tide); })
-                .attr("width", 50)
-                .attr("height", 30)
-                .style("stroke", 'pink')
-                .style("fill", 'pink')
-                .style("stroke-width", '1px')
-        }
+                .x(function (d) { return x(d.hour) })
+                .y(function (d) { return y(d.sun) })
+                .curve(d3.curveMonotoneX)
 
         const lineTideMain = () => {
             g.append('path')
@@ -62,6 +49,7 @@ export const Chart = (props) => {
                 .exit()
                 .remove('path')
         }
+
         const drawChart = () => {
             const currentWidth = parseInt(d3.select('.svgchart').style('width'), "100%")
             const currentHeight = parseInt(d3.select('.svgchart').style('height'), "100%")
@@ -70,7 +58,7 @@ export const Chart = (props) => {
             x.range([0, currentWidth * 6])
             xAxis.call(d3.axisBottom(x).ticks(2)).attr("transform", `translate(0,${currentHeight - 20})`)
             y.range([currentHeight - 20, 0]);
-            yAxis.call(d3.axisLeft(y).ticks(10))
+            yAxis.call(d3.axisLeft(y))
             var area = d3.area()
                 .x(function (d) { return x(d.hour); })
                 .y0(currentHeight - 20)
@@ -84,10 +72,214 @@ export const Chart = (props) => {
                 .attr('fill', '#0affe840')
             lineTideMain()
             lineSunMain()
-            rectangle()
+            //replace
+            g.append('rect')
+                .data(([props.chartData]))
+                .attr("class", 'rect-line')
+                .attr('x', function (d) { return x(d?.hour) })
+                .attr("y", function (d) { return y(d?.tide) })
+                .attr("width", currentWidth / 2)
+                .attr("height", 0)
+                .style("fill", 'gray')
+                .style("opacity", 0.3)
+                .attr("transform", `translate(0,${0})`)
+            //rect
+            g.append('rect')
+                .data(([props.chartData]))
+                .attr("class", 'rect-infor')
+                .attr('x', function (d) { return x(d[0]?.hour) })
+                .attr("y", function (d) { return y(d[0]?.tide) })
+                .attr("width", 70)
+                .attr("height", 50)
+
+            g.append('rect')
+                .data(([props.chartData]))
+                .attr("class", 'rect-infor_tidemax')
+                .attr('x', function (d) { return x(d[1]?.hour) })
+                .attr("y", function (d) { return y(d[1]?.tide) })
+                .attr("width", 70)
+                .attr("height", 50)
+
+            g.append('rect')
+                .data(([props.chartData]))
+                .attr("class", 'rect-infor')
+                .attr('x', function (d) { return x(d[2]?.hour) })
+                .attr("y", function (d) { return y(d[2]?.tide) })
+                .attr("width", 70)
+                .attr("height", 50)
+
+            g.append('rect')
+                .data(([props.chartData]))
+                .attr("class", 'rect-infor_tidemax')
+                .attr('x', function (d) { return x(d[3]?.hour) })
+                .attr("y", function (d) { return y(d[3]?.tide) })
+                .attr("width", 70)
+                .attr("height", 50)
+
+            g.append('rect')
+                .data(([props.chartData]))
+                .attr("class", 'rect-infor')
+                .attr('x', function (d) { return x(d[4]?.hour) })
+                .attr("y", function (d) { return y(d[4]?.tide) })
+                .attr("width", 70)
+                .attr("height", 50)
+
+            g.append('rect')
+                .data(([props.chartData]))
+                .attr("class", 'rect-infor_tidemax')
+                .attr('x', function (d) { return x(d[5]?.hour) })
+                .attr("y", function (d) { return y(d[5]?.tide) })
+                .attr("width", 70)
+                .attr("height", 50)
+
+            g.append('rect')
+                .data(([props.chartData]))
+                .attr("class", 'rect-infor')
+                .attr('x', function (d) { return x(d[6]?.hour) })
+                .attr("y", function (d) { return y(d[6]?.tide) })
+                .attr("width", 70)
+                .attr("height", 50)
+
+            g.append('rect')
+                .data(([props.chartData]))
+                .attr("class", 'rect-infor_tidemax')
+                .attr('x', function (d) { return x(d[7]?.hour) })
+                .attr("y", function (d) { return y(d[7]?.tide) })
+                .attr("width", 70)
+                .attr("height", 50)
+
+            g.append('rect')
+                .data(([props.chartData]))
+                .attr("class", 'rect-infor')
+                .attr('x', function (d) { return x(d[8]?.hour) })
+                .attr("y", function (d) { return y(d[8]?.tide) })
+                .attr("width", 70)
+                .attr("height", 50)
+
+            g.append('rect')
+                .data(([props.chartData]))
+                .attr("class", 'rect-infor_tidemax')
+                .attr('x', function (d) { return x(d[9]?.hour) })
+                .attr("y", function (d) { return y(d[9]?.tide) })
+                .attr("width", 70)
+                .attr("height", 50)
+
+            g.append('rect')
+                .data(([props.chartData]))
+                .attr("class", 'rect-infor')
+                .attr('x', function (d) { return x(d[10]?.hour) })
+                .attr("y", function (d) { return y(d[10]?.tide) })
+                .attr("width", 70)
+                .attr("height", 50)
+
+            g.append('rect')
+                .data(([props.chartData]))
+                .attr("class", 'rect-infor_tidemax')
+                .attr('x', function (d) { return x(d[11]?.hour) })
+                .attr("y", function (d) { return y(d[11]?.tide) })
+                .attr("width", 70)
+                .attr("height", 50)
+
+            g.append('rect')
+                .data(([props.chartData]))
+                .attr("class", 'rect-infor')
+                .attr('x', function (d) { return x(d[12]?.hour) })
+                .attr("y", function (d) { return y(d[12]?.tide) })
+                .attr("width", 70)
+                .attr("height", 50)
+
+            g.append('rect')
+                .data(([props.chartData]))
+                .attr("class", 'rect-infor_tidemax')
+                .attr('x', function (d) { return x(d[13]?.hour) })
+                .attr("y", function (d) { return y(d[13]?.tide) })
+                .attr("width", 70)
+                .attr("height", 50)
+
+            //[0]moon
+            g.append('rect')
+                .data(([props.chartData]))
+                .attr("class", 'rect-line')
+                .attr('x', function (d) { return x(d[0]?.hour) })
+                .attr("y", function (d) { return y(d[0]?.tide) })
+                .attr("width", currentWidth / 2)
+                .attr("height", currentHeight * 4)
+                .style("fill", 'gray')
+                .style("opacity", 0.3)
+                .attr("transform", `translate(0,${currentHeight - currentHeight * 3})`)
+            //[3],[4]
+            g.append('rect')
+                .data(([props.chartData]))
+                .attr("class", 'rect-line')
+                .attr('x', function (d) { return x(d[3]?.hour) })
+                .attr("y", function (d) { return y(d[3]?.tide) })
+                .attr("width", currentWidth / 2)
+                .attr("height", currentHeight * 4)
+                .style("fill", 'gray')
+                .style("opacity", 0.3)
+                .attr("transform", `translate(0,${currentHeight - currentHeight * 3})`)
+            g.append('rect')
+                .data(([props.chartData]))
+                .attr("class", 'rect-line')
+                .attr('x', function (d) { return x(d[4]?.hour) })
+                .attr("y", function (d) { return y(d[4]?.tide) })
+                .attr("width", currentWidth / 2)
+                .attr("height", currentHeight * 4)
+                .style("fill", 'gray')
+                .style("opacity", 0.3)
+                .attr("transform", `translate(0,${currentHeight - currentHeight * 3})`)
+            //[7],[8]
+            g.append('rect')
+                .data(([props.chartData]))
+                .attr("class", 'rect-line')
+                .attr('x', function (d) { return x(d[7]?.hour) })
+                .attr("y", function (d) { return y(d[7]?.tide) })
+                .attr("width", currentWidth / 2)
+                .attr("height", currentHeight * 4)
+                .style("fill", 'gray')
+                .style("opacity", 0.3)
+                .attr("transform", `translate(0,${currentHeight - currentHeight * 3})`)
+            g.append('rect')
+                .data(([props.chartData]))
+                .attr("class", 'rect-line')
+                .attr('x', function (d) { return x(d[8]?.hour) })
+                .attr("y", function (d) { return y(d[8]?.tide) })
+                .attr("width", currentWidth / 2)
+                .attr("height", currentHeight * 4)
+                .style("fill", 'gray')
+                .style("opacity", 0.3)
+                .attr("transform", `translate(0,${currentHeight - currentHeight * 3})`)
+            //[11],[12]
+            g.append('rect')
+                .data(([props.chartData]))
+                .attr("class", 'rect-line')
+                .attr('x', function (d) { return x(d[11]?.hour) })
+                .attr("y", function (d) { return y(d[11]?.tide) })
+                .attr("width", currentWidth / 2)
+                .attr("height", currentHeight * 4)
+                .style("fill", 'gray')
+                .style("opacity", 0.3)
+                .attr("transform", `translate(0,${currentHeight - currentHeight * 3})`)
+            g.append('rect')
+                .data(([props.chartData]))
+                .attr("class", 'rect-line')
+                .attr('x', function (d) { return x(d[12]?.hour) })
+                .attr("y", function (d) { return y(d[12]?.tide) })
+                .attr("width", currentWidth / 2)
+                .attr("height", currentHeight * 4)
+                .style("fill", 'gray')
+                .style("opacity", 0.3)
+                .attr("transform", `translate(0,${currentHeight - currentHeight * 3})`)
+        }
+        const rectRemove = () => {
+            g.selectAll('rect')
+                .data([(props.chartData)])
+                .exit()
+                .remove('rect')
         }
         const remove = () => {
             lineRemove()
+            rectRemove()
         }
         drawChart()
 
@@ -97,12 +289,12 @@ export const Chart = (props) => {
 
     return (
         <div className='layout__container-chart'>
+            <div className='svgchart'></div>
             <div className='layout__container-chart--title'>
-                <span className='blue-title'>Tide</span>
-                <span className='orange-title'>Sunrise & Sunset</span>
+                <span className='blue-title'>Tide •</span>
+                <span className='orange-title'>Sunrise &amp; Sunset</span>
             </div>
-            <div className='svgchart'>
-            </div>
+
         </div>
     )
 }
