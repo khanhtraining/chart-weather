f86e50e0b3184e14fe2c03bd2b5fe4f70def1e6e
ADMIN\Admin <huynhngockhanh0568@gmail.com>
2021-08-11 14:34:55 +0700

    Commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,8 @@
 import { render } from '@testing-library/react'
 import App from './App'
 
-const appContainer = () => render(<App />); describe('App Container test', () => {
+const appContainer = () => render(<App />)
+describe('App Container test', () => {
   test('renders app test', () => {
     const wrapper = appContainer()
     const titleDiv = wrapper.getByText('WIND SPEED')
