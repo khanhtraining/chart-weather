c7319a1886a74a692e61e0ff5d28c18266cda901
ADMIN\Admin <huynhngockhanh0568@gmail.com>
2021-08-11 11:55:24 +0700

    Commit code TodayDetail

---
+++ src/scenes/TodayDetail/TodayDetailCards/TodayDetailAdd.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const TodayDetailAdd = () => {
+    return (
+        <div className='todayDetailCards-wrapper-item'>
+            <div className='add-icon'></div>
+            <div className='add-text'>Add</div>
+        </div>
+    )
+}
+
+export default TodayDetailAdd
